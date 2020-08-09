cf441209ca2127eabfec405d311c474d08cd8e69
Santiago Pestarini <santiagonob@gmail.com>
2020-08-09 13:03:03 -0300

    fix name
    Signed-off-by: Santiago Pestarini <santiagonob@gmail.com>

--- django_project_heroku_template/settings.py
+++ django_project_heroku_template/settings.py
@@ -146,7 +146,7 @@ TEMPLATES[0]["DIRS"].append(BASE_DIR / "templates")
 # Deployment
 
 # Heroku
-HEROKUAPP_NAME = os.environ.get("DJANGO_HEROKUAPP_NAME", "my_new_clean_herokuapp")
+HEROKUAPP_NAME = os.environ.get("DJANGO_HEROKUAPP_NAME", "my-new-clean-herokuapp")
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "r+)1xlbz%lau$zpv$mbn#_6cy6rkc-eg!2@0s45jx!2ubuo3m1")
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 ALLOWED_HOSTS = [f"{HEROKUAPP_NAME}.herokuapp.com", "127.0.0.1"]
